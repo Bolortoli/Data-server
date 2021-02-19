a901cea8e1bd500d54ced59ae126a2f5b18bb466
bolortoli <bolortoli@csms.edu.mn>
2021-02-19 17:47:24 +0800

    Added session storage that saves data while server turned off

--- front-end/src/components/DataList.js
+++ front-end/src/components/DataList.js
@@ -2,13 +2,63 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const DataList = (props) => {
-   const [data, setData] = useState([1]);
+   const [data, setData] = useState([]);
+   const [input, setInput] = useState("");
+   const [isServerAlive, setIsServerAlive] = useState(true);
+
+   const postRequestToServer = (data) => {
+      axios
+         .post("http://127.0.0.1:8000", {
+            data: data,
+         })
+         .then((res) => {
+            setInput("");
+            setIsServerAlive(true);
+         })
+         .catch((error) => {
+            localStorage.setItem(
+               `pendingData${
+                  parseInt(localStorage.getItem("pendingDataCount")) + 1
+               }`,
+               input
+            );
+
+            localStorage.setItem(
+               "pendingDataCount",
+               `${parseInt(localStorage.getItem("pendingDataCount")) + 1}`
+            );
+            setInput("");
+            setIsServerAlive(false);
+         });
+   };
+
+   const sendToDatabase = () => {
+      postRequestToServer(input);
+      if (isServerAlive) {
+         for (
+            let i = 1;
+            i <= parseInt(localStorage.getItem("pendingDataCount"));
+            i++
+         ) {
+            if (localStorage.getItem(`pendingData${i}`) != null) {
+               console.log(localStorage.getItem(`pendingData${i}`));
+               postRequestToServer(localStorage.getItem(`pendingData${i}`));
+            }
+         }
+         fetchData();
+      }
+   };
+
+   const handleInputChange = (event) => {
+      setInput(event.target.value);
+   };
 
    const fetchData = () => {
       axios
-         .get("127.0.0.1:8000/")
+         .get("http://127.0.0.1:8000")
          .then((res) => {
-            alert(JSON.stringify(res));
+            console.log(res.data);
+            setData(res.data);
          })
          .catch((err) => {
             alert(JSON.stringify(err));
@@ -16,10 +66,37 @@ const DataList = (props) => {
    };
 
    useEffect(() => {
+      if (localStorage.getItem("pendingDataCount") == null)
+         localStorage.setItem("pendingDataCount", 0);
       fetchData();
    }, []);
 
-   return <>{JSON.stringify(data)}</>;
+   return (
+      <div
+         style={{ flex: 1, backgroundColor: "#506289", flexDirection: "row" }}
+      >
+         <div style={{ flex: 1, backgroundColor: "#058813" }}>
+            <ul>
+               {data.map((d) => (
+                  <li key={d.id}>
+                     <h4>{d.data}</h4>
+                     <h6> Added date: {new Date(d.date).toLocaleString()}</h6>
+                  </li>
+               ))}
+            </ul>
+         </div>
+         <div style={{ flex: 1, backgroundColor: "#058813" }}>
+            <label htmlFor="data-input">Type data to store database: </label>
+            <input
+               type="text"
+               id="data-input"
+               onChange={handleInputChange}
+               value={input}
+            />
+            <button onClick={sendToDatabase}>Send</button>
+         </div>
+      </div>
+   );
 };
 
 export default DataList;
